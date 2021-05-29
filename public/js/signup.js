96cbbc2f1d60d099a604c9a240be5c7861bf1ccb
Albert Mwasisoba <almwassy@gmail.com>
2021-05-29 12:12:04 +0300

    signup-responsive

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,42 +1,42 @@
 function switchToFormTwo() {
-    let inputObjectName = document.querySelector("#required_id_name");
-    let inputObjectEmail = document.querySelector("#required_id_email");
-    let inputObjectBirth = document.querySelector("#required_id_birthday")
+    let inputObjectName = document.getElementById("required_id_name");
+    let inputObjectEmail = document.getElementById("required_id_email");
+    let inputObjectBirth = document.getElementById("required_id_birthday")
 
     if (!inputObjectName.checkValidity()) {
-        document.querySelector("#validationMsgName").innerHTML = "Please fill out your Full Name";
-        document.querySelector("#validationMsgName").style.color = "red";
-        document.querySelector("#validationMsgName").style.fontSize = "10px";
+        document.getElementById("validationMsgName").innerHTML = "Please fill out your Full Name";
+        document.getElementById("validationMsgName").style.color = "red";
+        document.getElementById("validationMsgName").style.fontSize = "10px";
     } else if (!inputObjectEmail.checkValidity()) {
-        document.querySelector("#validationMsgEmail").innerHTML = "Please fill out your Email";
-        document.querySelector("#validationMsgEmail").style.color = "red";
-        document.querySelector("#validationMsgEmail").style.fontSize = "10px";
+        document.getElementById("validationMsgEmail").innerHTML = "Please fill out your Email";
+        document.getElementById("validationMsgEmail").style.color = "red";
+        document.getElementById("validationMsgEmail").style.fontSize = "10px";
     } else if (!inputObjectBirth.checkValidity()) {
-        document.querySelector("#validationMsgBirthday").innerHTML = "Please fill out your Birthday";
-        document.querySelector("#validationMsgBirthday").style.color = "red";
-        document.querySelector("#validationMsgBirthday").style.fontSize = "10px";
+        document.getElementById("validationMsgBirthday").innerHTML = "Please fill out your Birthday";
+        document.getElementById("validationMsgBirthday").style.color = "red";
+        document.getElementById("validationMsgBirthday").style.fontSize = "10px";
     } else {
-        document.querySelector("#formOne").style.display = "none";
-        document.querySelector("#formTwo").style.display = "block";
+        document.getElementById("formOne").style.display = "none";
+        document.getElementById("formTwo").style.display = "block";
     }
 }
 
 function switchToFormThree() {
-    let inputObjectMobile = document.querySelector("#required_id_two_mobile");
-    let inputObjectGender = document.querySelector("#required_id_two_gender");
-    let inputObjectCountry = document.querySelector("#required_id_two_country")
+    let inputObjectMobile = document.getElementById("required_id_two_mobile");
+    let inputObjectGender = document.getElementById("required_id_two_gender");
+    let inputObjectCountry = document.getElementById("required_id_two_country")
 
     if (!inputObjectMobile.checkValidity()) {
-        document.querySelector("#MsgMobile").innerHTML = "Please fill out your Mobile Number";
-        document.querySelector("#MsgMobile").style.color = "red";
+        document.getElementById("MsgMobile").innerHTML = "Please fill out your Mobile Number";
+        document.getElementById("MsgMobile").style.color = "red";
     } else if (!inputObjectGender.checkValidity()) {
-        document.querySelector("#MsgGender").innerHTML = "Please choose your gender";
-        document.querySelector("#MsgGender").style.color = "red";
+        document.getElementById("MsgGender").innerHTML = "Please choose your gender";
+        document.getElementById("MsgGender").style.color = "red";
     } else if (!inputObjectCountry.checkValidity()) {
-        document.querySelector("#MsgCountry").innerHTML = "Please choose your country";
-        document.querySelector("#MsgCountry").style.color = "red";
+        document.getElementById("MsgCountry").innerHTML = "Please choose your country";
+        document.getElementById("MsgCountry").style.color = "red";
     } else {
-        document.querySelector("#formTwo").style.display = "none";
-        document.querySelector("#formThree").style.display = "block";
+        document.getElementById("formTwo").style.display = "none";
+        document.getElementById("formThree").style.display = "block";
     }
 }
